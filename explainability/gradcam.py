dcffaa99e22668cd71290e3b3d2a43c439f1439d
ani18cs <aniketkumarsingh715@gmail.com>
2026-09-23 22:19:56 +0530

    Fix pytorch-grad-cam targets argument bug

--- explainability/gradcam.py
+++ explainability/gradcam.py
@@ -7,6 +7,6 @@ import numpy as np
 
 def get_gradcam_overlay(model, input_tensor, rgb_img, target_layer):
     cam = GradCAM(model=model, target_layers=[target_layer])
-    grayscale_cam = cam(input_tensor=input_tensor)[0]
+    grayscale_cam = cam(input_tensor=input_tensor, targets=None)[0]
     visualization = show_cam_on_image(rgb_img / 255.0, grayscale_cam, use_rgb=True)
     return visualization
